213cb7bb1616d1c9bf7143c077f2fd1783a46984
Alexey <alexeyinshakov992@gmail.com>
2024-12-20 16:41:35 +0300

    34 done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -353,8 +353,24 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const result = [];
+  let chunk = [];
+
+  arr.map((item, i) => {
+    if (chunk.length < chunkSize) {
+      chunk.push(item);
+    } else {
+      result.push(chunk);
+      chunk = [];
+      chunk.push(item);
+    }
+    if (i === arr.length - 1) {
+      result.push(chunk);
+    }
+    return undefined;
+  });
+  return result;
 }
 
 /**
