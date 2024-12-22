6aa2a531fb9bdb6fc3f887483a084d4487e85aa0
Alexey Inshakov <alexeyinshakov992@gmail.com>
2024-12-22 20:35:11 +0300

    47 done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -403,8 +403,13 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  let target = [...arr];
+  indices.map((i) => {
+    target = target[i];
+    return undefined;
+  });
+  return target;
 }
 
 /**
